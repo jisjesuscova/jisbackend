ff2a54bc8cf28ee6abc0aa082461d289553d5616
jisjesuscova <jesuscova@jisparking.com>
2023-10-02 12:35:25 -0300

    cambios

--- app/backend/routers/banks.py
+++ app/backend/routers/banks.py
@@ -1,12 +1,8 @@
-from fastapi import APIRouter, Depends, Request, Response, UploadFile, File
+from fastapi import APIRouter, Depends
 from app.backend.db.database import get_db
 from sqlalchemy.orm import Session
-from app.backend.schemas import Employee, UpdateEmployee, SearchEmployee, UserLogin, EmployeeList, UploadSignature, UploadPicture
-from app.backend.classes.employee_class import EmployeeClass
+from app.backend.schemas import UserLogin, EmployeeList
 from app.backend.auth.auth_user import get_current_active_user
-import base64
-import os
-from app.backend.classes.dropbox_class import DropboxClass
 
 banks = APIRouter(
     prefix="/banks",
